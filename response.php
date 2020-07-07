457b99c5499f38d367177099fbbf4c6fbfecc522
pengilen <67626423+pengilen@users.noreply.github.com>
2020-07-07 16:17:08 +0700

    Update response.php

--- response.php
+++ response.php
@@ -2,13 +2,19 @@
 
 $a = array();
 $obj1 = (object)array();
+$obj2 = (object)array();
 
 $obj1->type = 0;
-$obj1->content = 'My Title';
+$obj1->content = "1594112711 My Array Test Title 5";
 $obj1->bold = 1;
 $obj1->align = 2;
 $obj1->format = 3;
 array_push($a,$obj1);
+
+$obj2->type = 1;
+$obj2->path = "https://www.matetech.in/myfiles/1.jpg";
+$obj2->align = 2;
+array_push($a,$obj2);
 
 echo json_encode($a,JSON_FORCE_OBJECT);
 
